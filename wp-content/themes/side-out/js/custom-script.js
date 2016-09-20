2d5d4706b4e107d7df83dda2b34ba6cf62e76112
root <root@ip-172-31-14-149.ap-northeast-2.compute.internal>
2016-09-20 08:26:07 +0000

    init2

--- wp-content/themes/side-out/js/custom-script.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($){
-    $('.sideout-color-field').wpColorPicker();
-});
--- wp-content/themes/side-out/sidebar.php
+++
@@ -1,3 +0,0 @@
-                <?php if(is_active_sidebar( 'primary-sidebar' ) ) : ?>
-                    <?php dynamic_sidebar( 'primary-sidebar' ); ?> 
-                <?php endif; ?>
